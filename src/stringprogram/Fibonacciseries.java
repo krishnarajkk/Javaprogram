116c4cc537eb4bff366b0b46b28284b8985f8ed0
krishnarajkk <kkrishnaraj183@gmail.com>
2025-03-03 16:31:06 +0530

    End of program

--- src/stringprogram/Fibonacciseries.java
+++ src/stringprogram/Fibonacciseries.java
@@ -2,15 +2,14 @@ package stringprogram;
 
 public class Fibonacciseries {
 	public static void main(String[] args) {
-		long l=0, l1=1;
-		for(int i=0;i<=500;i++)
+		int l=0, l1=1;
+		for(int i=0;i<=9;i++)
 		{
-			System.out.println("New Line added");
 			System.out.print(l+" ");
-			long l2=l+l1;
+			int l2=l+l1;
 			l=l1;
 			l1=l2;
-			System.out.println("Code updated");
 			}
+		System.out.println("End of Program");
 	}
 }
